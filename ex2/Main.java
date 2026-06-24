36bf75d349a2de2af6b5705622732e64c1a442a9
Davi <davisendin@gmail.com>
2026-06-24 16:13:49 -0300

    justifiquei o ex2

---
+++ ex2/Main.java
@@ -0,0 +1,11 @@
+package ex2;
+
+//Template e uma superclasse para que a classe filha possa criar sobre
+//Decorator pega uma classe e acrescenta em cima dela
+//A diferenca e que template e uma base enquanto decorator e acrescentado em cima da classe original
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
